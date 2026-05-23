e005c8fd17eebb6e79d36ba36746663f6255a3bf
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-05-23 10:10:33 +0600

    made the users and the issues table

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,54 @@
-import {Pool} from 'pg'
+import { Pool } from 'pg'
 import config from '../config/config'
 
 const pool = new Pool({
-    connectionString : config.connectionString
+    connectionString: config.connectionString
 });
+
+
+export const initDB = async () => {
+    try {
+
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role VARCHAR(50) DEFAULT 'contributor'
+        CHECK(role IN ('contributor', 'maintainer')),
+
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+         );
+    `);
+
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS issues(
+        id SERIAL PRIMARY KEY,
+
+        title VARCHAR(150) NOT NULL,
+        description TEXT NOT NULL
+        CHECK(char_length(description) >= 20),
+
+        type VARCHAR(100) NOT NULL
+        CHECK(type IN ('bug', 'feature_request')),
+        status VARCHAR(30) DEFAULT 'open'
+        CHECK(status IN ('open', 'in_progress', 'resolved')),
+        reporter_id INT NOT NULL,
+
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+        );
+    `);
+
+    console.log(`Database is connected successfully!`);
+
+    } catch (err: any) {
+        if (err instanceof Error) {
+            throw new Error(err.message)
+        }
+        throw new Error('Something went wrong while initializing Database!')
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import app from "./app"
 import config from "./config/config"
+import { initDB } from "./database"
 
 const main = ()=>{
+    initDB(); //? initializing the database in the begining
+    
     app.listen(config.port, ()=>{
         console.log(`This app is listening from port number : ${config.port}`);
     })
